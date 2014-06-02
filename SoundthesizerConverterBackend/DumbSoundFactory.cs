ffac8a5a2cd0bcc934c0cc23334e699cb99b43a8
AndreasKarg <Clonkman@gmx.de>
2014-06-02 19:21:48 +0200

    Cleanup: Extract sound factory method to separate class

---
+++ SoundthesizerConverterBackend/DumbSoundFactory.cs
@@ -0,0 +1,31 @@
+﻿using SoundthesizerConverterBackend.Xml;
+
+namespace SoundthesizerConverterBackend
+{
+  public static class DumbSoundFactory
+  {
+    public static ISound GenerateFromSoundthesizer(soundsetSound sound)
+    {
+      IDependency volume, pan, pitch;
+      //TODO: Rework Trigger Dependency handling via decorator
+      IDependency trigger;
+
+      try
+      {
+
+        volume = DumbDependencyFactory.GenerateFromFile(sound.volume, "volume");
+        pan = DumbDependencyFactory.GenerateFromFile(sound.pan, "pan");
+        pitch = DumbDependencyFactory.GenerateFromFile(sound.frequency, "frequency");
+
+        trigger = DumbDependencyFactory.GenerateFromFile(sound.trigger, "trigger");
+      }
+      catch (SoundthesizerFileFormatException e)
+      {
+        throw new SoundthesizerFileFormatException(string.Format("An error occured while converting sound '{0}'", sound.name), e);
+      }
+
+      //TODO: Pass on trigger properly!
+      return new DumbSound(sound.name, sound.file.name, volume, pan, pitch, null);
+    }
+  }
+}
--- SoundthesizerConverterBackend/SoundsetFactory.cs
+++ SoundthesizerConverterBackend/SoundsetFactory.cs
@@ -8,35 +8,11 @@ namespace SoundthesizerConverterBackend
 {
   public static class DumbSoundsetFactory
   {
-    public static ISound GenerateSoundFromFile(soundsetSound sound)
-    {
-      IDependency volume, pan, pitch;
-      //TODO: Rework Trigger Dependency handling via decorator
-      IDependency trigger;
-
-      try
-      {
-
-        volume = DumbDependencyFactory.GenerateFromFile(sound.volume, "volume");
-        pan = DumbDependencyFactory.GenerateFromFile(sound.pan, "pan");
-        pitch = DumbDependencyFactory.GenerateFromFile(sound.frequency, "frequency");
-
-        trigger = DumbDependencyFactory.GenerateFromFile(sound.trigger, "trigger");
-      }
-      catch (SoundthesizerFileFormatException e)
-      {
-        throw new SoundthesizerFileFormatException(string.Format("An error occured while converting sound '{0}'", sound.name), e);
-      }
-
-      //TODO: Pass on trigger properly!
-      return new DumbSound(sound.name, sound.file.name, volume, pan, pitch, null);
-    }
-
-    public static SoundSet GenerateSoundsetFromFile(string filename)
+    public static SoundSet GenerateFromSoundthesizer(string filename)
     {
       var soundSet = soundset.LoadFromFile(filename);
 
-      var sounds = soundSet.Sounds.Select(GenerateSoundFromFile).ToList().AsReadOnly();
+      var sounds = soundSet.Sounds.Select(DumbSoundFactory.GenerateFromSoundthesizer).ToList().AsReadOnly();
 
       return new SoundSet(sounds, soundSet.name);
     }
