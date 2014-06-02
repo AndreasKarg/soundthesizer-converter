e2c3198835527efa4dcf501f863cb575cce8151b
AndreasKarg <Clonkman@gmx.de>
2014-06-03 00:45:10 +0200

    Aux: Reliably get path to test files

--- SoundthesizerConverterBackendTests/FileFormatTests.cs
+++ SoundthesizerConverterBackendTests/FileFormatTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.CodeDom;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SoundthesizerConverterBackend;
 using SoundthesizerConverterBackend.DumbTypes;
@@ -11,10 +14,21 @@ namespace SoundthesizerConverterBackendTests
   [TestClass]
   public class SoundthesizerFileFormatTests
   {
+    public TestContext TestContext { get; set; }
+
+    private string GetTestFilePath(string filename)
+    {
+      var directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+      Debug.Assert(directoryName != null);
+
+      return Path.Combine(directoryName, "Test Files", filename);
+    }
+
     [TestMethod]
     public void LoadBR423TestFile()
     {
-      var result = Soundset.LoadFromFile("Test Files\\BR423.xml");
+      var result = Soundset.LoadFromFile(GetTestFilePath("BR423.xml"));
 
       Assert.AreEqual("BR 423", result.Name);
       Assert.AreEqual(9, result.Sounds.Count);
@@ -24,7 +38,7 @@ namespace SoundthesizerConverterBackendTests
     [TestMethod]
     public void LoadEmptyTestFile()
     {
-      var result = Soundset.LoadFromFile("Test Files\\EmptyTestFile.xml");
+      var result = Soundset.LoadFromFile(GetTestFilePath("EmptyTestFile.xml"));
 
       Assert.AreEqual("BR 423", result.Name);
     }
@@ -33,13 +47,13 @@ namespace SoundthesizerConverterBackendTests
     [ExpectedException(typeof(InvalidOperationException), "The value 'FAIL' for the 'operator' attribute in the test file was inappropriately allowed.")]
     public void LoadEnumTestFile()
     {
-      Soundset.LoadFromFile("Test Files\\EnumTestFile.xml");
+      Soundset.LoadFromFile(GetTestFilePath("EnumTestFile.xml"));
     }
 
     [TestMethod]
     public void GenerateDumbSoundSetFromBR423()
     {
-      var soundSet = DumbSoundsetFactory.GenerateFromSoundthesizer("Test Files\\BR423.xml");
+      var soundSet = DumbSoundsetFactory.GenerateFromSoundthesizer(GetTestFilePath("BR423.xml"));
       Assert.AreEqual("BR 423", soundSet.Name);
       Assert.AreEqual(9, soundSet.Sounds.Count);
       Assert.IsTrue(Math.Abs(71 - ((soundSet.Sounds.ElementAt(1).Volume as IArithmeticDependency).Operands.ElementAt(1) as IValueDependency).RefPoints.ElementAt(4).X) < 1);
