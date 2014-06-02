f8db87a884282f881fa80cfc5a8672c7149c0f6e
AndreasKarg <Clonkman@gmx.de>
2014-06-02 19:14:46 +0200

    Cleanup: Extract Dependency factory methods to separate class

---
+++ SoundthesizerConverterBackend/DumbDependencyFactory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SoundthesizerConverterBackend.Xml;
+
+namespace SoundthesizerConverterBackend
+{
+  public static class DumbDependencyFactory
+  {
+    public static IDependency GenerateFromSoundthesizer(Dependency dep)
+    {
+      if (dep == null)
+        return null;
+
+      if (dep.value == "none")
+        return null;
+
+      if (dep.operatorSpecified && (dep.value != null))
+        throw new SoundthesizerFileFormatException("Operator and Value must not be defined for the same Dependency.");
+
+      //Hack: As the deserializer will always create an empty object even when the respective element is absent in the source file, assume an empty dependency to be absent.
+      if (!dep.operatorSpecified && (dep.value == null) && (dep.refpoint.Count == 0) && (dep.dependency.Count == 0))
+        return null;
+
+      if (!dep.operatorSpecified && (dep.value == null))
+        throw new SoundthesizerFileFormatException("Either Operator or Value must be defined for a Dependency.");
+
+      return dep.value != null ? GenerateValueTypeFromSoundthesizer(dep) : GenerateArithmeticTypeFromSoundthesizer(dep);
+    }
+
+    private static IDependency GenerateArithmeticTypeFromSoundthesizer(Dependency dep)
+    {
+      if(dep.dependency.Count == 0)
+        throw new SoundthesizerFileFormatException("A Dependency with the 'operator' attribute set must contain at least one child Dependency.");
+
+      var children = new List<IDependency>();
+
+      for (int i = 0; i < dep.dependency.Count; i++)
+      {
+        try
+        {
+          children.Add(GenerateFromSoundthesizer(dep.dependency[i]));
+        }
+        catch (SoundthesizerFileFormatException e)
+        {
+          throw new SoundthesizerFileFormatException(String.Format("An error occured while converting Dependency {0}", i), e);
+        }
+      }
+
+      //TODO: Actually convert the operator
+      return new DumbArithmeticDependency(children.AsReadOnly(), Operator.Add);
+    }
+
+    private static IDependency GenerateValueTypeFromSoundthesizer(Dependency dep)
+    {
+      if(dep.refpoint.Count == 0)
+        throw new SoundthesizerFileFormatException("A Dependency with the 'value' attribute set must contain at least one refpoint.");
+
+      var refpoints = dep.refpoint.Select(x => new DoublePoint(x.x, x.y)).ToList().AsReadOnly();
+
+      //TODO: Actually convert Value type to enum
+      return new DumbValueDependency(refpoints, InputType.Bla);
+    }
+
+    public static IDependency GenerateFromFile(Dependency dep, string dependencyName)
+    {
+      try
+      {
+        return GenerateFromSoundthesizer(dep);
+      }
+      catch (SoundthesizerFileFormatException e)
+      {
+        throw new SoundthesizerFileFormatException(string.Format("An error occured while converting property '{0}'", dependencyName), e);
+      }
+    }
+  }
+}
--- SoundthesizerConverterBackend/SoundsetFactory.cs
+++ SoundthesizerConverterBackend/SoundsetFactory.cs
@@ -8,73 +8,6 @@ namespace SoundthesizerConverterBackend
 {
   public static class DumbSoundsetFactory
   {
-    public static IDependency GenerateDependencyFromFile(Dependency dep)
-    {
-      if (dep == null)
-        return null;
-
-      if (dep.value == "none")
-        return null;
-
-      if (dep.operatorSpecified && (dep.value != null))
-        throw new SoundthesizerFileFormatException("Operator and Value must not be defined for the same Dependency.");
-
-      //Hack: As the deserializer will always create an empty object even when the respective element is absent in the source file, assume an empty dependency to be absent.
-      if (!dep.operatorSpecified && (dep.value == null) && (dep.refpoint.Count == 0) && (dep.dependency.Count == 0))
-        return null;
-
-      if (!dep.operatorSpecified && (dep.value == null))
-        throw new SoundthesizerFileFormatException("Either Operator or Value must be defined for a Dependency.");
-
-      return dep.value != null ? GenerateValueDependencyFromFile(dep) : GenerateArithmeticDependencyFromFile(dep);
-    }
-
-    private static IDependency GenerateArithmeticDependencyFromFile(Dependency dep)
-    {
-      if(dep.dependency.Count == 0)
-        throw new SoundthesizerFileFormatException("A Dependency with the 'operator' attribute set must contain at least one child Dependency.");
-
-      var children = new List<IDependency>();
-
-      for (int i = 0; i < dep.dependency.Count; i++)
-      {
-        try
-        {
-          children.Add(GenerateDependencyFromFile(dep.dependency[i]));
-        }
-        catch (SoundthesizerFileFormatException e)
-        {
-          throw new SoundthesizerFileFormatException(String.Format("An error occured while converting Dependency {0}", i), e);
-        }
-      }
-
-      //TODO: Actually convert the operator
-      return new DumbArithmeticDependency(children.AsReadOnly(), Operator.Add);
-    }
-
-    private static IDependency GenerateValueDependencyFromFile(Dependency dep)
-    {
-      if(dep.refpoint.Count == 0)
-        throw new SoundthesizerFileFormatException("A Dependency with the 'value' attribute set must contain at least one refpoint.");
-
-      var refpoints = dep.refpoint.Select(x => new DoublePoint(x.x, x.y)).ToList().AsReadOnly();
-
-      //TODO: Actually convert Value type to enum
-      return new DumbValueDependency(refpoints, InputType.Bla);
-    }
-
-    private static IDependency GenerateDependencyFromFileWithExceptionDecoration(Dependency dep, string soundProperty)
-    {
-      try
-      {
-        return GenerateDependencyFromFile(dep);
-      }
-      catch (SoundthesizerFileFormatException e)
-      {
-        throw new SoundthesizerFileFormatException(string.Format("An error occured while converting property '{0}'", soundProperty), e);
-      }
-    }
-
     public static ISound GenerateSoundFromFile(soundsetSound sound)
     {
       IDependency volume, pan, pitch;
@@ -84,11 +17,11 @@ namespace SoundthesizerConverterBackend
       try
       {
 
-        volume = GenerateDependencyFromFileWithExceptionDecoration(sound.volume, "volume");
-        pan = GenerateDependencyFromFileWithExceptionDecoration(sound.pan, "pan");
-        pitch = GenerateDependencyFromFileWithExceptionDecoration(sound.frequency, "frequency");
+        volume = DumbDependencyFactory.GenerateFromFile(sound.volume, "volume");
+        pan = DumbDependencyFactory.GenerateFromFile(sound.pan, "pan");
+        pitch = DumbDependencyFactory.GenerateFromFile(sound.frequency, "frequency");
 
-        trigger = GenerateDependencyFromFileWithExceptionDecoration(sound.trigger, "trigger");
+        trigger = DumbDependencyFactory.GenerateFromFile(sound.trigger, "trigger");
       }
       catch (SoundthesizerFileFormatException e)
       {
